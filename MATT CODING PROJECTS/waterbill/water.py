be13fcbc552e3e439af67217edb6349dec7ecc35
MATTBOOK\Matt Ploszanski <intromatt@gmail.com>
2018-01-29 18:42:38 +0200

    Water Bill Calculator

---
+++ MATT CODING PROJECTS/waterbill/water.py
@@ -0,0 +1,12 @@
+latest = int(input ("Enter Latest Reading - "))
+last = int(input ("Enter Previous Reading - "))
+total = latest-last
+leva = total*2.5
+euro = leva/2
+beer = leva/1.5
+print("The Amount of Water Used In Cubic Meters In the Apartment -"),
+print(total)
+print("The Amount Owing in Bulgarian Leva -"),
+print(leva)
+print("The Amount Owing in Euro -"),
+print(euro)
